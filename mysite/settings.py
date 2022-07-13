a3209c6a95b180c8daf6adee58b551fcde39f84d
pradigcorp <hyuma17@yahoo.co.jp>
2022-07-13 15:01:36 -0300

    MBO1

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MBO.apps.MBOconfig'
+    'MBO.apps.MboConfig'
 ]
 
 MIDDLEWARE = [
